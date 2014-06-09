20d8cc56ace53508e3d1d8bb146cff9fed57d31e
Daiki Tamada <daiki.tamada@gmail.com>
2014-06-09 13:24:20 +0900

    Added new usb interface (Buffalo WI-U2-433DM)

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -287,6 +287,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x7392, 0xA812),.driver_info = RTL8821}, /* Edimax - Edimax */
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x0846, 0x9052),.driver_info = RTL8821}, /* Netgear - A6100 */
+	{USB_DEVICE(0x0411, 0x0242),.driver_info = RTL8821}, /* BUFFALO - Edimax */
 #endif
 
 #ifdef CONFIG_RTL8192E
