a462d83e53cdc3a881c34aba91043414ecc4a743
Joe <josepha48@gmail.com>
2015-07-13 14:38:36 -0700

    add OUTLINK vendor id

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -289,6 +289,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
         {USB_DEVICE_AND_INTERFACE_INFO(USB_VENDER_ID_REALTEK, 0x0820,0xff,0xff,0xff),.driver_info = RTL8821}, /* 8821AU */
 	/*=== Customer ID ===*/
 	{USB_DEVICE(0x7392, 0xA811),.driver_info = RTL8821}, /* Edimax - Edimax */
+	{USB_DEVICE(0x0BDA, 0xA811),.driver_info = RTL8821}, /* OUTLINK - Edimax */ 
 	{USB_DEVICE(0x7392, 0xA812),.driver_info = RTL8821}, /* Edimax - Edimax */
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x0846, 0x9052),.driver_info = RTL8821}, /* Netgear - A6100 */
