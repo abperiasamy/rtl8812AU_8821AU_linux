1f21bd013ef7bc3eb418b2ac55a8d706b4812c91
Harshavardhana <harsh@harshavardhana.net>
2015-12-19 19:55:02 -0800

    Add support BUFFALO WI-U3-866D
    Fixes #100

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -279,6 +279,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x2357, 0x0101),.driver_info = RTL8812}, /* TP-Link - T4U */
 	{USB_DEVICE(0x148F, 0x9097),.driver_info = RTL8812}, /* Amped Wireless ACA1 */
         {USB_DEVICE(0x2357, 0x0103),.driver_info = RTL8812}, /* TP-Link - T4UH */
+        {USB_DEVICE(0x0411, 0x025D),.driver_info = RTL8821}, /* BUFFALO WI-U3-866D */
 #endif
 
 #ifdef CONFIG_RTL8821A
